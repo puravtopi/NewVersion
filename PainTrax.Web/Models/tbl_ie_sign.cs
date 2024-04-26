cab3d39283081b9154d353c108e4497a15eac22b
GoUp <GoUp@DESKTOP-72PCTDR>
2024-04-26 17:38:23 +0530

    signature Pad

---
+++ PainTrax.Web/Models/tbl_ie_sign.cs
@@ -0,0 +1,10 @@
+﻿namespace PainTrax.Web.Models
+{
+    public class tbl_ie_sign
+    {
+        public int? id { get; set; }
+        public int? ie_id { get; set; }
+        public int? patient_id { get; set; }
+        public string? signatureData { get; set; }
+    }
+}

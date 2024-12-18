7aa484b3be14382c64e7e9912cb1b1b6ac905dda
saranya-1991 <ssaranyamca15@gmail.com>
2024-12-18 17:52:20 +0530

    Intake form design changes completed

---
+++ PainTrax.Web/Controllers/IntakeFormController.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using Microsoft.AspNetCore.Mvc;
+using PainTrax.Web.Filter;
+using PainTrax.Web.Helper;
+using PainTrax.Web.Models;
+using PainTrax.Web.Services;
+using System.Data;
+using System.Text.RegularExpressions;
+
+namespace PainTrax.Web.Controllers
+{
+    public class IntakeFormController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            tbl_locations obj = new tbl_locations();
+            return View(obj);
+        }
+    }
+}

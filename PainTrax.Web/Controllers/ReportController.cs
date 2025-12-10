a1c8af68b5bc4aecbe45dc4b246087952d831afc
Purav <purav.topi@gmail.com>
2025-12-10 12:16:09 +0530

    Task : Fixed POC report rest option issue.

--- PainTrax.Web/Controllers/ReportController.cs
+++ PainTrax.Web/Controllers/ReportController.cs
@@ -93,7 +93,7 @@ namespace PainTrax.Web.Controllers
             else if (mcodeid == 3)
             {
                // query += " and pp.INhouseProcbit<>1 and  ISNULL(pp.Other,0) <> 1  ";
-                query += " and pp.INhouseProcbit<>1 and ISNULL(pp.inout<>1) and  ISNULL(pp.Other,0) <> 1  ";
+                query += " and ISNULL(pp.INhouseProcbit,0)<>1 and ISNULL(pp.inout1,0)<>1 and  ISNULL(pp.Other,0) <> 1  ";
             }
             else if (mcodeid == 4)
             {
